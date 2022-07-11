393dd9eddcb5afecfd19cde6f45f66cc0e4fae9f
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-11 00:39:29 +0000

    saved all files. completed string-manipulation-1

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,7 +1,12 @@
 /* exported isVowel */
 
 /*
-- new variable needs to
+- all char values must firt be converted into a capital letter to
+make this easier, and they must be saved into a new variable
+- a conditional statment must be declared that checks to see
+- if the new variable is strictly equal to 1 of 6 vowels
+- if true for any, return true
+- if false, return false
 
 */
 function isVowel(char) {
