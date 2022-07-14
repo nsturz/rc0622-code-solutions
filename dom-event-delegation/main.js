b56f60589bb010a8b7ea28c61d00361be092275f
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-14 21:47:55 +0000

    completed dom-event-delegation exercise

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -1,16 +1,13 @@
 /* function e() {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
-}
-
+} */
 var taskList = document.querySelector('.task-list');
-var button = document.querySelectorAll('button');
 
 taskList.addEventListener('click', function (event) {
-  if (event.target === button) {
+  if (event.target.tagName === 'BUTTON') {
     var closest = event.target.closest('.task-list-item');
     console.log('closest:', closest);
     closest.remove();
   }
 });
-*/
