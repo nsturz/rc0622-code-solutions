ccaef0326723c529eb70fcd0348c7cc59652ede2
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-14 19:56:21 +0000

    almost finsished

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,16 @@
+/* function e() {
+  console.log('event.target:', event.target);
+  console.log('event.target.tagName:', event.target.tagName);
+}
+
+var taskList = document.querySelector('.task-list');
+var button = document.querySelectorAll('button');
+
+taskList.addEventListener('click', function (event) {
+  if (event.target === button) {
+    var closest = event.target.closest('.task-list-item');
+    console.log('closest:', closest);
+    closest.remove();
+  }
+});
+*/
