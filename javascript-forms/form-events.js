e16bd3c2e0fbf6f1f674a45a0e020a53c6f15a4d
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-14 02:43:14 +0000

    updated form-values.js

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -14,29 +14,9 @@ function handleInput(event) {
     'targetValue:', event.target.value);
 }
 
-function formReset() {
-  document.getElementById('contact-form').reset();
-}
-
-function onSubmit(event) {
-  event.preventDefault();
-  var name = contactForm.elements.name.value;
-  var email = contactForm.elements.email.value;
-  var textArea = contactForm.elements.message.value;
-  var formData = {};
-  formData.name = name;
-  formData.email = email;
-  formData.textArea = textArea;
-
-  formReset();
-  console.log(formData);
-}
-
 var userName = document.getElementById('user-name');
 var userEmail = document.getElementById('user-email');
 var userTextArea = document.getElementById('user-message');
-var contactForm = document.getElementById('contact-form');
-var button = document.querySelector('button');
 
 userName.addEventListener('focus', handleFocus);
 userName.addEventListener('blur', handleBlur);
@@ -49,5 +29,3 @@ userEmail.addEventListener('input', handleInput);
 userTextArea.addEventListener('focus', handleFocus);
 userTextArea.addEventListener('blur', handleBlur);
 userTextArea.addEventListener('input', handleInput);
-button.addEventListener('click', onSubmit);
-contactForm.addEventListener('submit', onSubmit);
