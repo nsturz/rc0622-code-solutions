baab3f198ba4da33e0b88c899ed4cdd8aeec8bf7
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-25 18:54:48 +0000

    completed javascript-this exercise

---
+++ javascript-this/student.js
@@ -0,0 +1,14 @@
+/* exported student */
+var student = {
+  firstName: 'Nick',
+  lastName: 'Sturz',
+  subject: 'nerd stuff',
+  getFullName: function () {
+    return this.firstName + ' ' + this.lastName;
+  },
+  getIntroduction: function () {
+    return 'Hello, my name is ' +
+    this.firstName + ' ' + this.lastName + ' and I am studying ' +
+    this.subject + '.';
+  }
+};
