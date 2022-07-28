2f59449ad61f7236ec9a4e95c9b686d751491416
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-28 00:42:18 +0000

    photo swapping function now works

---
+++ javascript-carousel/main.js
@@ -0,0 +1,25 @@
+// you now need to create the functionality that turns each little
+// "dot" black as the photos change!!!
+
+// function photoSwap() {
+//   var image = document.querySelector('img');
+//   if (image.getAttribute('src') === 'images/001.png') {
+//     var charmander = image.setAttribute('src', 'images/004.png');
+//     return charmander;
+//   } else if (image.getAttribute('src') === 'images/004.png') {
+//     var squirtle = image.setAttribute('src', 'images/007.png');
+//     return squirtle;
+//   } else if (image.getAttribute('src') === 'images/007.png') {
+//     var pikachu = image.setAttribute('src', 'images/025.png');
+//     return pikachu;
+//   } else if (image.getAttribute('src') === 'images/025.png') {
+//     var jigglyPuff = image.setAttribute('src', 'images/039.png');
+//     return jigglyPuff;
+//   } else if (image.getAttribute('src') === 'images/039.png') {
+//     image.setAttribute('src', 'images/001.png');
+//   }
+// }
+
+// setInterval(photoSwap, 3000);
+
+// console.log(image.getAttribute('src'));
