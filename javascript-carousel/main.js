1d973beac8b10f21bc5a2c4ec5e0e8fe8a6b8e86
Nicolas Sturz <nicksturz92@gmail.com>
2022-07-29 00:58:11 +0000

    working on event listeners

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,25 +1,83 @@
-// you now need to create the functionality that turns each little
-// "dot" black as the photos change!!!
+// you now need to edit the event listener for the side buttons
+var dot1 = document.getElementById('dot-1');
+var dot2 = document.getElementById('dot-2');
+var dot3 = document.getElementById('dot-3');
+var dot4 = document.getElementById('dot-4');
+var dot5 = document.getElementById('dot-5');
+var image = document.querySelector('img');
+// var leftButton = document.getElementById('left-button');
+// var rightButton = document.getElementById('right-button');
 
-// function photoSwap() {
-//   var image = document.querySelector('img');
-//   if (image.getAttribute('src') === 'images/001.png') {
-//     var charmander = image.setAttribute('src', 'images/004.png');
-//     return charmander;
-//   } else if (image.getAttribute('src') === 'images/004.png') {
-//     var squirtle = image.setAttribute('src', 'images/007.png');
-//     return squirtle;
-//   } else if (image.getAttribute('src') === 'images/007.png') {
-//     var pikachu = image.setAttribute('src', 'images/025.png');
-//     return pikachu;
-//   } else if (image.getAttribute('src') === 'images/025.png') {
-//     var jigglyPuff = image.setAttribute('src', 'images/039.png');
-//     return jigglyPuff;
-//   } else if (image.getAttribute('src') === 'images/039.png') {
-//     image.setAttribute('src', 'images/001.png');
-//   }
+function dotSwap(pokemon) {
+  if (pokemon === 'bulbasaur') {
+    dot1.className = 'fa-solid fa-circle fa-1x';
+    dot2.className = 'fa-regular fa-circle fa-1x';
+    dot3.className = 'fa-regular fa-circle fa-1x';
+    dot4.className = 'fa-regular fa-circle fa-1x';
+    dot5.className = 'fa-regular fa-circle fa-1x';
+  } else if (pokemon === 'charmander') {
+    dot1.className = 'fa-regular fa-circle fa-1x';
+    dot2.className = 'fa-solid fa-circle fa-1x';
+    dot3.className = 'fa-regular fa-circle fa-1x';
+    dot4.className = 'fa-regular fa-circle fa-1x';
+    dot5.className = 'fa-regular fa-circle fa-1x';
+  } else if (pokemon === 'squirtle') {
+    dot1.className = 'fa-regular fa-circle fa-1x';
+    dot2.className = 'fa-regular fa-circle fa-1x';
+    dot3.className = 'fa-solid fa-circle fa-1x';
+    dot4.className = 'fa-regular fa-circle fa-1x';
+    dot5.className = 'fa-regular fa-circle fa-1x';
+  } else if (pokemon === 'pikachu') {
+    dot1.className = 'fa-regular fa-circle fa-1x';
+    dot2.className = 'fa-regular fa-circle fa-1x';
+    dot3.className = 'fa-regular fa-circle fa-1x';
+    dot4.className = 'fa-solid fa-circle fa-1x';
+    dot5.className = 'fa-regular fa-circle fa-1x';
+  } else if (pokemon === 'jigglypuff') {
+    dot1.className = 'fa-regular fa-circle fa-1x';
+    dot2.className = 'fa-regular fa-circle fa-1x';
+    dot3.className = 'fa-regular fa-circle fa-1x';
+    dot4.className = 'fa-regular fa-circle fa-1x';
+    dot5.className = 'fa-solid fa-circle fa-1x';
+  }
+}
+
+function photoSwap(pokemon) {
+
+  if (
+    image.getAttribute('src') === 'images/001.png' || pokemon === 'charmander') {
+    image.setAttribute('src', 'images/004.png');
+    dotSwap('charmander');
+  } else if (
+    image.getAttribute('src') === 'images/004.png' || pokemon === 'squirtle') {
+    image.setAttribute('src', 'images/007.png');
+    dotSwap('squirtle');
+  } else if (
+    image.getAttribute('src') === 'images/007.png' || pokemon === 'pikachu') {
+    image.setAttribute('src', 'images/025.png');
+    dotSwap('pikachu');
+  } else if (
+    image.getAttribute('src') === 'images/025.png' || pokemon === 'jigglypuff') {
+    image.setAttribute('src', 'images/039.png');
+    dotSwap('jigglypuff');
+  } else if (
+    image.getAttribute('src') === 'images/039.png' || pokemon === 'bulbasaur') {
+    image.setAttribute('src', 'images/001.png');
+    dotSwap('bulbasaur');
+  }
+}
+
+// function clear() {
+//   clearInterval(interval);
 // }
 
-// setInterval(photoSwap, 3000);
+// leftButton.addEventListener('click', function (event) {
+//   clear();
+
+// });
+
+photoSwap('squirtle');
+// var interval = setInterval(photoSwap, 3000);
+// console.log(setInterval(photoSwap, 3000));
 
 // console.log(image.getAttribute('src'));
