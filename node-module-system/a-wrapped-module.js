db31bd17a5ab319eccb720318ae7147117180b32
Nicolas Sturz <nicksturz92@gmail.com>
2022-08-17 20:15:42 +0000

    completed node-module-system exercise.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log('exports typeof:', typeof exports);
+console.log('value of exports: ', exports);
+
+console.log('require typeof:', typeof require);
+console.log('value of require: ', require);
+
+console.log('module typeof:', typeof module);
+console.log('value of module: ', module);
+
+console.log('_filename typeof:', typeof _filename);
+console.log('value of _filename: ', __filename);
+
+console.log('_dirname typeof:', typeof _dirname);
+console.log('value of _dirname: ', __dirname);
