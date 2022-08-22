8cca6e12c473ce284646934d9a1b8fa00671e708
Nicolas Sturz <nicksturz92@gmail.com>
2022-08-22 17:08:37 +0000

    still need to finish this exercise

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -1,3 +1,12 @@
-// const express = require('./node_modules/express');
+// const express = require('express');
 
 // const app = express();
+
+// app.use((req, res) => {
+//   console.log('method property:,', req.method);
+//   console.log('send property:', req.send('Sending!!!'));
+// });
+
+// app.listen(3000, () => {
+//   console.log('Listening on Port 3000!');
+// });
