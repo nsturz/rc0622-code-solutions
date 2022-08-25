7edaa02c108c1c75a235f2ba7ad3f819b8db4e69
Nicolas Sturz <nicksturz92@gmail.com>
2022-08-25 16:39:06 +0000

    completed express-hello-world exercise

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -1,12 +1,11 @@
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// app.use((req, res) => {
-//   console.log('method property:,', req.method);
-//   console.log('send property:', req.send('Sending!!!'));
-// });
+app.use((req, res) => {
 
-// app.listen(3000, () => {
-//   console.log('Listening on Port 3000!');
-// });
+});
+
+app.listen(3000, () => {
+
+});
