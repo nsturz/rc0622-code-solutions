c2dd3a238c1fe64f3c7638d7a1a6b483bab7076a
Nicolas Sturz <nicksturz92@gmail.com>
2022-09-01 17:36:56 +0000

    fixed one error, now there are terminal errors

--- react-class-components/src/index.jsx
+++ react-class-components/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-class CustomButton extends React.component {
+class CustomButton extends React.Component {
   render() {
     return (
       <button>{this.props.text}</button>
