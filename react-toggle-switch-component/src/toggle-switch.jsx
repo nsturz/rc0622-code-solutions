a63638ae1d99eec82199df85f5a31b6854e51f73
Nicolas Sturz <nicksturz92@gmail.com>
2022-09-02 22:44:02 +0000

    completed react-toggle-switch-exercise

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 
 export default class ToggleSwitch extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.state({ button: 'on' });
+  constructor(props) {
+    super(props);
+    this.state = { click: 1 };
+    this.increment = this.increment.bind(this);
 
-  // }
+  }
+
+  increment() {
+    this.setState({ click: this.state.click + 1 });
+  }
 
   render() {
+    let offButton = 'hidden';
+    let onButton = 'on-button relative';
+    let containerStatus = 'delay container-on';
+    const click = this.state.click;
+    if (click % 2 === 0) {
+      offButton = '';
+      onButton = 'on-button relative hidden';
+      containerStatus = 'delay container-off';
+    }
+
     return (
-      <div className="flex container-on">
-        <button className="off hidden">OFF</button>
-        <button className="on">ON</button>
+      <div className={containerStatus}>
+        <button className={offButton} onClick={this.increment}>OFF</button>
+        <button className={onButton} onClick={this.increment}>ON</button>
       </div>
     );
   }
