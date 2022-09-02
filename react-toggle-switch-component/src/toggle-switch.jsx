ec52903b7fe278e99fcb0c76e62dfa24170a678a
Nicolas Sturz <nicksturz92@gmail.com>
2022-09-02 20:28:20 +0000

    finishing up styling for container div

--- react-toggle-switch-component/src/toggle-switch.jsx
+++ react-toggle-switch-component/src/toggle-switch.jsx
@@ -1,8 +1,18 @@
-// import React from 'react';
+import React from 'react';
 
-// export default class ToggleSwitch extends React.component {
-//   constructor(props) {
-//     super(props);
+export default class ToggleSwitch extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   // this.state({ button: 'on' });
 
-//   }
-// }
+  // }
+
+  render() {
+    return (
+      <div className="flex container-on">
+        <button className="off hidden">OFF</button>
+        <button className="on">ON</button>
+      </div>
+    );
+  }
+}
