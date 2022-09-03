2c55fc02d3e6847e2e71eb377d9bd7587df26725
Nicolas Sturz <nicksturz92@gmail.com>
2022-09-03 00:50:10 +0000

    saving

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -3,18 +3,33 @@ import React from 'react';
 export default class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { timer: 0 };
+    this.state = { timer: 0, click: 1 };
+    this.state.increment = this.increment.bind(this);
+  }
+
+  increment() {
+    this.setState({ click: this.state.click + 1 });
   }
 
   render() {
+    const click = this.state.click;
+    // eslint-disable-next-line no-console
+    console.log(click);
+    let playButton = 'fa solid fa-play play-button';
+    let pauseButton = 'fa-solid fa-pause pause-button hidden';
+    if (click % 2 === 0) {
+      playButton = 'fa-solid fa-play play-button hidden';
+      pauseButton = 'fa-solid fa-pause pause-button';
+
+    }
     return (
       <div>
         <div className="container">
           <p className="number">{this.state.timer}</p>
         </div>
         <div className="icon-wrapper">
-          <i className="fa-solid fa-play play-button"></i>
-          <i className="fa-solid fa-pause pause-button hidden"></i>
+          <i className={playButton} onClick={this.increment}></i>
+          <i className={pauseButton} onClick={this.increment}></i>
         </div>
       </div>
     );
