fed6b4a848258ca9cc3fe262c5d693fd40042a04
Nicolas Sturz <nicksturz92@gmail.com>
2022-12-21 19:41:38 +0000

    completed array-manipulation-2 exercise

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,16 +1,26 @@
 /* exported chunk */
 /*
-- some notes: array.length / size should equal 1
+- completed challenge!!! did not use array.prototype.slice()
+- some notes: array.length / size should not equal zero exfept for the very last problem
 - there is an original array, and a final array, with an unknown
 number of subsequent arrays contained in the final array.
--
-
 */
 
 function chunk(array, size) {
-  var finalArray = [];
-  var sliced = array.slice(0, size);
-  var sliced2 = array.slice(-size);
-  finalArray.push(sliced, sliced2);
-  return finalArray;
+  if (array.length !== 0) {
+    var finalArray = [];
+    var subArray = [];
+    for (var i = 0; i < array.length; i++) {
+      if (subArray.length < size) {
+        subArray.push(array[i]);
+      } else if (subArray.length >= size) {
+        finalArray.push(subArray);
+        subArray = [];
+        subArray.push(array[i]);
+      }
+    }
+    finalArray.push(subArray);
+    return finalArray;
+  }
+  return array;
 }
