617c6f022d5053d46f3dad296d7730ca45488f83
Nicolas Sturz <nicksturz92@gmail.com>
2023-01-26 02:26:08 +0000

    completed javascript-typing-tutor exercise

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-console */
-// letters now turn green when correct, and red when incorrect, but they also turn red when you
-// press "space" after a correct letter
-
 var span = document.querySelectorAll('span');
 var counter = 0;
 
@@ -11,18 +7,11 @@ document.addEventListener('keydown', event => {
   }
   for (var i = 0; i < span.length; i++) {
     if (event.key === span[i].textContent && counter - 1 === i) {
-      span[i].className = 'activated';
-      console.log('i', i);
-      console.log('counter:', counter);
-    } else if (event.key !== span[i].textContent && counter - 1 === i) {
-      span[i].className = 'wrong';
-
+      span[i].className = 'correct';
+      span[i + 1].className = 'underline';
+    } else if (event.key !== span[i].textContent && counter - 1 === i && event.key !== ' ') {
+      span[i].className = 'wrong underline';
+      counter--;
     }
   }
-  // // eslint-disable-next-line no-console
-  // span[0].className = 'activated';
-  //   console.log('span:', span);
-  //   console.log('span.textContent', span[0].textContent);
-  // console.log('event.key', typeof event.key);
-  // console.log('event:', event);
 });
